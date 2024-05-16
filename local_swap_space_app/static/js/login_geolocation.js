d40f1184139ee96f3afba8f248a674bf6f9072d9
Sebastian Szewczyk <147999792+wallHack0@users.noreply.github.com>
2024-05-16 21:22:58 +0200

    Update login_geolocation.js

--- local_swap_space_app/static/js/login_geolocation.js
+++ local_swap_space_app/static/js/login_geolocation.js
@@ -1,21 +1,21 @@
-// Nasłuchiwanie zdarzenia 'submit' na formularzu logowania.
+// Listening for the 'submit' event on the login form.
 document.getElementById('loginForm').addEventListener('submit', function (event) {
-    // Sprawdzenie, czy geolokalizacja jest dostępna w przeglądarce użytkownika.
+    // Checking if geolocation is available in the user's browser.
     if (navigator.geolocation) {
-        // Zatrzymanie domyślnego przetwarzania formularza, aby najpierw pobrać dane geolokalizacyjne.
+        // Stopping the default form processing to first fetch geolocation data
         event.preventDefault();
 
-        // Pobiera aktualne współrzędne geograficzne użytkownika.
+        // Fetching the current geographic coordinates of the user.
         navigator.geolocation.getCurrentPosition(function (position) {
             let latitude = position.coords.latitude;
             let longitude = position.coords.longitude;
 
-            // Stworzenie elementów input, które będą przechowywać szerokość i długość geograficzną.
+            // Creating input elements to store latitude and longitude.
             let hiddenLat = document.createElement('input');
             hiddenLat.setAttribute('type', 'hidden');
             hiddenLat.setAttribute('name', 'latitude');
             hiddenLat.value = latitude;
-            // Dołączenie tego elementu do formularza logowania.
+            // Attaching this element to the login form.
             document.getElementById('loginForm').appendChild(hiddenLat);
 
             let hiddenLong = document.createElement('input');
@@ -24,12 +24,12 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
             hiddenLong.value = longitude;
             document.getElementById('loginForm').appendChild(hiddenLong);
 
-            // Ponowne przesłanie formularza, teraz z dodanymi danymi geograficznymi.
+            // Resubmitting the form, now with added geographic data.
             document.getElementById('loginForm').submit();
 
         }, function (error) {
             console.log("Błąd: ", error);
-            // Przesłanie formularza mimo błędów związanych z lokalizacją.
+            // Submitting the form even if there are errors related to geolocation.
             document.getElementById('loginForm').submit();
         });
     }
